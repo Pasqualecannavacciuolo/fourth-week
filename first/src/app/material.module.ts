5b20f11cddf468afb9841d48bd4cb751fd86ae65
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-11 11:51:26 +0200

    Added material module

---
+++ first/src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [CommonModule, MatToolbarModule],
+  exports: []
+})
+export class AppModule { }
