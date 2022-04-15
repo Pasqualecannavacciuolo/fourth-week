80bc6bbca59ec09f52fb46eba6b9c39103016a2e
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-15 11:09:20 +0200

    Added button click state

--- REACT/first-project/src/Link.js
+++ REACT/first-project/src/Link.js
@@ -2,7 +2,18 @@ import React from "react"
 
 export default class Link extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {counter: 0}
+    }
+
     render() {
-        return ( <div>{this.props.obj.name} {this.props.obj.lastname}</div>)
+        return ( 
+            <><div>{this.props.obj.name} {this.props.obj.lastname}</div><button onClick={() => {
+                this.setState({ counter: this.state.counter + 1 });
+            } } /><p>Hai cliccato
+                    {this.state.counter}
+                </p></>
+        );
     }
 }
