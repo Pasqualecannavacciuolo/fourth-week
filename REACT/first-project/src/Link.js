35cd0195bac47408e338dca5e1a7def0a2449f67
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-15 09:53:46 +0200

    Ignore

--- REACT/first-project/src/Link.js
+++ REACT/first-project/src/Link.js
@@ -3,6 +3,6 @@ import React from "react"
 export default class Link extends React.Component {
 
     render() {
-        return ( <div>{this.props.obj.name}  {this.props.obj.lastname}</div>)
+        return ( <div>{this.props.obj.name} {this.props.obj.lastname}</div>)
     }
 }
